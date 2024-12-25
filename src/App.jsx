94c5667363eebb19c1c36d12ec2fb42709072cea
chihiro <chieeie@gmail.com>
2024-12-25 20:03:19 +0900

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,23 @@ import "./App.css";
 import { getAllRecords } from "./utils/supabaseFunctions";
 
 export const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [records, setRecords] = useState([]);
 
   useEffect(() => {
     const getRecords = async () => {
       const records = await getAllRecords();
       setRecords(records);
+      setIsLoading(false);
     };
     getRecords();
   }, []);
 
   const [totalTime, setTotalTime] = useState(0);
   useEffect(() => {
-    const getRecords = async () => {
-      const records = await getAllRecords();
-      if (records.length > 0) {
-        const time = records.map((record) => record.time);
-        const total = time.reduce((a, b) => {
-          return a + b, 0;
-        });
-        setTotalTime(total);
-      }
-    };
-    getRecords();
+    const time = records.map((record) => Number(record.time));
+    const total = time.reduce((a, b) => a + b, 0);
+    setTotalTime(total);
   }, [records]);
 
   const [inputTitle, setInputTitle] = useState("");
@@ -50,51 +44,55 @@ export const App = () => {
     }
   };
 
-  return (
-    <>
-      <h1>学習記録一覧</h1>
-      <div className="">
-        <div>
-          <span>学習内容</span>
-          <input type="text" value={inputTitle} onChange={onChangeTitle} />
+  if (isLoading) {
+    return <p style={{ margin: "0 20px" }}>Loading...</p>;
+  } else {
+    return (
+      <>
+        <h1>学習記録一覧</h1>
+        <div className="">
+          <div>
+            <span>学習内容</span>
+            <input type="text" value={inputTitle} onChange={onChangeTitle} />
+          </div>
+          <div>
+            <span>学習時間</span>
+            <input
+              type="number"
+              min={0}
+              value={inputTime}
+              onChange={onChangeTime}
+            />
+            <span>時間</span>
+          </div>
         </div>
-        <div>
-          <span>学習時間</span>
-          <input
-            type="number"
-            min={0}
-            value={inputTime}
-            onChange={onChangeTime}
-          />
-          <span>時間</span>
+        <div className="">
+          <div>
+            <span>入力されている学習内容：</span>
+            <span>{inputTitle}</span>
+          </div>
+          <div>
+            <span>入力されている時間：</span>
+            <span>{inputTime}</span>
+          </div>
         </div>
-      </div>
-      <div className="">
-        <div>
-          <span>入力されている学習内容：</span>
-          <span>{inputTitle}</span>
+        <div className="">
+          <ul>
+            {records.map((record, id) => {
+              return (
+                <li key={id}>
+                  <p>
+                    {record.title}　{record.time}時間
+                  </p>
+                </li>
+              );
+            })}
+          </ul>
+          <button onClick={onClickAddRecord}>登録</button>
+          {error && <p>{error}</p>}
+          <p>合計時間：{totalTime}/1000h</p>
         </div>
-        <div>
-          <span>入力されている時間：</span>
-          <span>{inputTime}</span>
-        </div>
-      </div>
-      <div className="">
-        <ul>
-          {records.map((record, index) => {
-            return (
-              <li key={index}>
-                <p>
-                  {record.title}　{record.time}時間
-                </p>
-              </li>
-            );
-          })}
-        </ul>
-        <button onClick={onClickAddRecord}>登録</button>
-        {error && <p>{error}</p>}
-        <p>合計時間：{totalTime}/1000h</p>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 };
